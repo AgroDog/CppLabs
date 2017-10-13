aca72d4a466e082aeda8a5b03dc2ccdfc3fb8cb2
Vasiliy Chernyakov <menime13@rambler.ru>
2017-10-13 19:39:40 +0500

    lab 2

---
+++ Lab2Task1.cpp
@@ -0,0 +1,49 @@
+// Task1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int n;
+	cout << "array size = ";
+	cin >> n;
+	int *a = new int[n];
+	int even = 0;
+	int odd = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "[" << i + 1 << "]:";
+		cin >> a[i];
+		if (a[i] % 2 == 0)
+		{
+			even++;
+		}
+		else
+		{
+			odd++;
+		}
+	}
+	if (even == odd)
+	{
+		int result = 1;
+		for (int i = 0; i < n; i++)
+		{
+			result = result * (a[i] + a[n - 1 - i]);
+		}
+		cout << "result = " << result << endl;
+	}
+	else
+	{
+		cout << "even != odd" << endl;
+	}
+	delete[] a;
+	system("pause");
+
+    return 0;
+}
+
---
+++ Lab2Task2.cpp
@@ -0,0 +1,46 @@
+// Task2.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cout << "array size = ";
+	cin >> n;
+	int *a = new int[n];
+	for (int i = 0; i < n; i++) 
+	{ 
+		cout << "[" << i + 1 << "]:";
+		cin >> a[i];
+	}
+	int imax = 0;
+	for (int i = 0; i < n; i++) 
+	{
+		if (a[i] > a[imax])
+		{
+			imax = i;
+		}
+	}
+	for (int i = 0; i < imax; i++) 
+	{
+		if ((i+1) % 2 == 0) 
+		{
+			a[i] = a[i] * a[imax];
+		}
+	}
+
+	cout << "new array:" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "[" << i + 1 << "]:" << a[i] << endl;
+	}
+
+	delete[] a;
+	system("pause");
+    return 0;
+}
+
